0df826a320b040d0da653142696aeb8de86ce298
DucTuan1802 <105335784+DucTuan1802@users.noreply.github.com>
2022-05-11 13:14:50 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+var express = require("express");
+var app = express();
+var server = require("http").createServer(app);
+var io = require("socket.io")(server);
+var fs = require("fs");
+server.listen(process.env.PORT || 3000);
+
+console.log('Server Running');
+
+var arrayUser = [];
+var existFlag = true;
+io.sockets.on('connection',function(socket){
+	console.log('User connected');
+
+	socket.on('client-send-data',function(data){
+		console.log("server receive:" + data);
+		//io.sockets.emit('server-send-data',{content:data});
+	});
+	//---------- user register --------------//
+	socket.on('client-register-user',function(data){
+		if(arrayUser.indexOf(data) == -1){
+			// khong ton tai user
+			arrayUser.push(data);
+			existFlag = false;
+			console.log("register success:" + data);
+			//gan ten user cho socket
+			socket.un = data;
+			// send registed user
+			io.sockets.emit('server-send-user',{userList:arrayUser});
+		}else{
+			console.log("user exist:" + data);
+			existFlag = true;
+		}
+		//send register user
+		socket.emit('server-send-result',{result:existFlag});
+		//io.sockets.emit('server-send-data',{content:data});
+	});
+	//------- client send chat-------------//
+	socket.on('client-send-chat',function(message){
+		console.log(socket.un + ": " + message);
+		io.sockets.emit('server-send-chat',{chatContent:socket.un + ": " + message});
+	});
+
+});
