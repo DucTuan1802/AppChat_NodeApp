281d55e79d0af00e52733de27528ee24873ac244
DucTuan1802 <105335784+DucTuan1802@users.noreply.github.com>
2022-05-11 14:06:42 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 var server = require("http").createServer(app);
-var io = require("socket.io")(server);
+var io = require("socket.io")listen(server);
 var fs = require("fs");
 server.listen(process.env.PORT || 3000);
 
